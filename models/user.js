c511c8710f03f57f612e7e99a10c403701bbe5b1
Emelie Olsson <emelieannaolsson@gmail.com>
2022-02-03 12:07:49 +0100

    fix

--- models/user.js
+++ models/user.js
@@ -19,19 +19,4 @@ date :{
 });
 const User= mongoose.model('User',UserSchema);
 
-
-// Here's the mongoose plugin being used on a Model
-
-    Schema = mongoose.Schema
-    auditLog = require('audit-log');
- 
-var HumanSchema = new Schema({
-    name: { type: String },
-    height: { type: Number }
-});
- 
-var pluginFn = auditLog.getPlugin('mongoose', {modelName:'Human', namePath:'name'}); // setup occurs here
-HumanSchema.plugin(pluginFn.handler); // .handler is the pluggable function for mongoose in this case
-
-
 module.exports = User;
